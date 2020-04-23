e6a31862aab28f95426e8d2b0bcefe6eff7756b0
Filip Klapil <klapi.filip@seznam.cz>
2020-04-23 13:49:20 +0200

    fix commit

--- pc6/cv6_btn_care/cv6_btn_care.sim/sim_1/behav/xsim/xsim.dir/TB_btn_in_behav/obj/xsim_1.c
+++ pc6/cv6_btn_care/cv6_btn_care.sim/sim_1/behav/xsim/xsim.dir/TB_btn_in_behav/obj/xsim_1.c
@@ -48,24 +48,29 @@
 #endif
 typedef void (*funcp)(char *, char *);
 extern int main(int, char**);
-extern void execute_36(char*, char *);
-extern void execute_37(char*, char *);
+extern void execute_41(char*, char *);
+extern void execute_42(char*, char *);
 extern void execute_26(char*, char *);
-extern void execute_35(char*, char *);
+extern void execute_40(char*, char *);
 extern void execute_29(char*, char *);
 extern void execute_31(char*, char *);
 extern void execute_32(char*, char *);
 extern void execute_34(char*, char *);
+extern void execute_35(char*, char *);
+extern void execute_36(char*, char *);
+extern void execute_37(char*, char *);
+extern void execute_38(char*, char *);
+extern void execute_39(char*, char *);
 extern void transaction_0(char*, char*, unsigned, unsigned, unsigned);
 extern void vhdl_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
-funcp funcTab[10] = {(funcp)execute_36, (funcp)execute_37, (funcp)execute_26, (funcp)execute_35, (funcp)execute_29, (funcp)execute_31, (funcp)execute_32, (funcp)execute_34, (funcp)transaction_0, (funcp)vhdl_transfunc_eventcallback};
-const int NumRelocateId= 10;
+funcp funcTab[15] = {(funcp)execute_41, (funcp)execute_42, (funcp)execute_26, (funcp)execute_40, (funcp)execute_29, (funcp)execute_31, (funcp)execute_32, (funcp)execute_34, (funcp)execute_35, (funcp)execute_36, (funcp)execute_37, (funcp)execute_38, (funcp)execute_39, (funcp)transaction_0, (funcp)vhdl_transfunc_eventcallback};
+const int NumRelocateId= 15;
 
 void relocate(char *dp)
 {
-	iki_relocate(dp, "xsim.dir/TB_btn_in_behav/xsim.reloc",  (void **)funcTab, 10);
-	iki_vhdl_file_variable_register(dp + 5168);
-	iki_vhdl_file_variable_register(dp + 5224);
+	iki_relocate(dp, "xsim.dir/TB_btn_in_behav/xsim.reloc",  (void **)funcTab, 15);
+	iki_vhdl_file_variable_register(dp + 6064);
+	iki_vhdl_file_variable_register(dp + 6120);
 
 
 	/*Populate the transaction function pointer field in the whole net structure */
