8025cb0ca8e8f0e9cfb8f920c7d43fd41be07921
Unknown <klapil.filip@seznam.cz>
2020-05-12 10:00:09 +0200

    initial commit test exam

---
+++ zkouska_test/zkouska_test.srcs/sources_1/new/TOP.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 12.05.2020 09:55:36
+-- Design Name: 
+-- Module Name: TOP - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity TOP is
+    Port ( vstup : in STD_LOGIC);
+end TOP;
+
+architecture Behavioral of TOP is
+
+begin
+
+
+end Behavioral;
